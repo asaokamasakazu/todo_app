1a82c624813e750e0a9188edb614f1db22e916ca
m.asaoka <asaokamasakazu@yahoo.co.jp>
2023-01-02 22:05:45 +0900

    add: ユーザー情報編集用のアクションを追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -11,5 +12,36 @@ class UserController extends Controller
     public function show($id) {
         $user = User::find($id);
         return view('auth.show', compact('user'));
+    }
+
+    // 編集画面の表示
+    public function edit($id) {
+        $user = User::find($id);
+
+        if (auth()->id() != $user->id) {
+            return redirect('/');
+        }
+
+        return view('auth.edit', compact('user'));
+    }
+
+    // 登録情報の更新
+    public function update(Request $request, $id) {
+        $user = User::find($id);
+
+        if (auth()->id() != $user->id) {
+            return redirect('/');
+        }
+
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+            // 'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $form = $request->all();
+        unset($form['_token']);
+        $user->fill($form)->save();
+        return redirect()->route('user.show', $user);
     }
 }
