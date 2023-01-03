07ba900eeb94e88e59021f799ea0e95f2eeb6096
m.asaoka <asaokamasakazu@yahoo.co.jp>
2023-01-03 17:05:28 +0900

    add: task関連のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
@@ -15,3 +16,5 @@ Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('user.edit
 Route::patch('/users/{id}', [UserController::class, 'update'])->name('user.update');
 
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
+Route::resource('tasks', TaskController::class);
