436e1cb0c066eeb65e581fbf68f48ef25cf70e17
m.asaoka <asaokamasakazu@yahoo.co.jp>
2023-01-03 09:36:42 +0900

    add: ユーザー削除用のルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/users/{id}', [UserController::class, 'show'])->name('user.show');
 
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('user.edit');
 Route::patch('/users/{id}', [UserController::class, 'update'])->name('user.update');
+
+Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('user.destroy');
