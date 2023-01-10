e6f7f00aa22483548f89d1422fe96f7c4b6fded7
m.asaoka <asaokamasakazu@yahoo.co.jp>
2023-01-10 13:54:39 +0900

    add: フラッシュを追加

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,6 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
+            session()->flash('errorMessage', 'ログインが必要です。');
             return route('login');
         }
     }
